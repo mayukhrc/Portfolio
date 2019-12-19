2d5895b3551e5434a4a48df711e00551ede8326b
mayukhrc95 <mayukhrc1995@gmail.com>
2019-12-19 16:08:08 -0600

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "My blog post is this. Plaese read this. I am writing this blog post"
+		)
+end
+
+5.times do |skill|
+	Skill.create!(
+		title: "Skill #{skill}",
+		percent_utilized: 10
+		)
+end
+
+9.times do |portfolio_item|
+	MyPortfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		body: "Text body needed",
+		main_image: "https://place-hold.it/600x400",
+		thumb_image: "https://place-hold.it/300x200"
+		)
+end
