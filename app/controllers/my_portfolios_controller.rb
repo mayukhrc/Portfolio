174fdad84aa14ccd279a7d2bc715a0c562f5a0f2
mayukhrc95 <mayukhrc1995@gmail.com>
2019-12-22 20:12:57 -0600

    Portfolio items added

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -2,4 +2,18 @@ class MyPortfoliosController < ApplicationController
 	def index
 		@portfolio=MyPortfolio.all
 	end
+	def new
+		@portfolio = MyPortfolio.new
+	end
+	def create
+    @portfolio = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to my_portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
