dd8987cc98ccff755f5913f1454373cc02e8696f
mayukhrc95 <mayukhrc1995@gmail.com>
2019-12-24 04:11:06 -0600

    custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get 'my_portfolio/:id', to: 'my_portfolios#show', as: 'my_portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do 
+  	member do
+  		get :toggle_status
+  	end
+  end
+
 end
