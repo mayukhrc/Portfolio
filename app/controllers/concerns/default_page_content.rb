97378ded2b1103326edcb5593d79036333530a12
mayukhrc95 <mayukhrc1995@gmail.com>
2020-01-04 07:27:18 -0600

    Added application views

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+
+	extend ActiveSupport::Concern
+
+	included do
+		before_action :set_page_defaults
+	end
+
+	def set_page_defaults
+		@page_title = "Portfolio"
+		@seo_keywords = "Mayukh RoyChowdhury Portfolio"
+	end
+end
